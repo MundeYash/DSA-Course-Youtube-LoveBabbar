f097a9655de8bef72d2e14d769f82a7de37ef39f
Yash Munde <yashmunde68@gmail.com>
2026-01-10 23:20:46 +0530

    removing unnecessary files

--- LECTURE-10/3sum.cpp
+++ LECTURE-10/3sum.cpp
@@ -1,33 +1,32 @@
-#include<bits/stdc++.h>
-using namespace std ; 
-// 3Sum question : 
-void threeSum(vector<int>&a , int n , int target  ){
-    for (int i=0 ;i<n-2 ;i++){
-        for (int j=(i+1) ;j<n-1 ;j++){
-            for (int k =(j+1) ;k<n ;k++){
-                int sum = a[i] +a[j] + a[k ] ; 
-                if (sum == target ){
-                     cout<<a[i]<<" "<<a[j]<<" "<<a[k]<<"\n";
-
-                  
-                   
-
+#include <bits/stdc++.h>
+using namespace std;
+// 3Sum question :
+void threeSum(vector<int> &a, int n, int target)
+{
+    for (int i = 0; i < n - 2; i++)
+    {
+        for (int j = (i + 1); j < n - 1; j++)
+        {
+            for (int k = (j + 1); k < n; k++)
+            {
+                int sum = a[i] + a[j] + a[k];
+                if (sum == target)
+                {
+                    cout << a[i] << " " << a[j] << " " << a[k] << "\n";
                 }
             }
         }
-
     }
-
 }
-int main(){
-    vector<int>a = {10 ,5 ,5 ,5 ,2} ;
-    sort(a.begin() , a.end());
-    int n = a.size() ; 
-    int k = 12 ;
-   threeSum(a,n,k);
+int main()
+{
+    vector<int> a = {10, 5, 5, 5, 2};
+    sort(a.begin(), a.end());
+    int n = a.size();
+    int k = 12;
+    threeSum(a, n, k);
 
-
-    return 0 ;
+    return 0;
 }
 
 /*
